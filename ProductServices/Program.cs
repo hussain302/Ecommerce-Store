a8ef550578b74e8a611b477041f16c950ca05b18
Muhammad.Hussain <Muhammad.Hussain@MUHAMMADHUSSAIN>
2023-08-13 20:30:54 +0500

    API Gateway code pushed

--- ProductServices/Program.cs
+++ ProductServices/Program.cs
@@ -32,5 +32,4 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapGet("/", () => "Welcome to Products API project of Microservices");
-app.MapControllers();
 app.Run();
